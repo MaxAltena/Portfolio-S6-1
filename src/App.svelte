64751a7ef0babc102bc81213463ffb286ecbe035
Max Altena <max@maxaltena.com>
2020-02-20 07:19:05 +0100

    Updated styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,50 @@
 <script>
+	import Clock from "./Clock.svelte";
+
+	let csCurrent = window.appPreferences.colorScheme.current;
+	let rmCurrent = window.appPreferences.reducedMotion.current;
+
+	function handleCSClick() {
+		window.appPreferences.colorScheme.toggle();
+		csCurrent = window.appPreferences.colorScheme.current;
+	}
+
+	function handleRMClick() {
+		window.appPreferences.reducedMotion.toggle();
+		rmCurrent = window.appPreferences.reducedMotion.current;
+	}
+
+	export let title;
 	export let name;
 </script>
 
 <style>
 	main {
 		text-align: center;
+		font-family: sans-serif;
 	}
 
 	h1 {
-		text-transform: uppercase;
+		font-size: 2rem;
+	}
+
+	button {
+		font-size: 1rem;
 	}
 </style>
 
 <main>
-	<h1>BYE {name}!</h1>
-	<p>
-		Visit the
-		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
-	<button on:click={() => window.appPreferences.colorScheme.set('dark')}>cs to dark</button>
-	<button on:click={() => window.appPreferences.colorScheme.set('light')}>cs to light</button>
-	<button on:click={() => window.appPreferences.colorScheme.reset()}>cs to none</button>
-	<button on:click={() => window.appPreferences.reducedMotion.set('reduce')}>rm to reduce</button>
-	<button on:click={() => window.appPreferences.reducedMotion.reset()}>rm to none</button>
-	<button on:click={() => window.appPreferences.colorScheme.toggle()}>toggle cs</button>
-	<button on:click={() => window.appPreferences.reducedMotion.toggle()}>toggle rm</button>
+	<h1>{title} – {name}</h1>
+
+	<div>
+		<button on:click={handleCSClick}>Toggle theme</button>
+		<p>{csCurrent}</p>
+	</div>
+
+	<div>
+		<button on:click={handleRMClick}>Toggle animations</button>
+		<p>{rmCurrent}</p>
+	</div>
+
+	<Clock />
 </main>
