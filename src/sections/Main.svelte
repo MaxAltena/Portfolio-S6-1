d5210e1832eb24e78bbaa1494b9799747322f1df
Max Altena <Max@MaxAltena.com>
2020-04-01 22:19:49 +0200

    updated some styling

--- src/sections/Main.svelte
+++ src/sections/Main.svelte
@@ -10,6 +10,7 @@
 	p {
 		color: var(--accent);
 		transition: all var(--transition-speed) var(--transition-timing);
+		margin: 0.5rem 0;
 	}
 
 	@media only screen and (max-width: 450px){
@@ -24,9 +25,13 @@
 
 	<hr />
 
-	<p>Lorem ipsum van {data.name}</p>
+	<p>Dit is pas een mooi portfolio van {data.firstName} {data.lastName}</p>
 
 	<hr />
+
+	<p>Lorem ipsum dolor sit amet, eu quo intellegat constituam. Ne integre habemus nam, duo conceptam constituto id. Ea sed doctus admodum lucilius. Ad solum accommodare duo.</p>
+
+	<p>Eum idque graece ei, eros vidit erroribus cum an. Duo id mucius vocent suscipiantur, ad pertinacia dissentiunt mel. Id sit facilis sadipscing complectitur, ei vis minim eirmod neglegentur. Ad fugit sonet eam, ne cibo idque vix, erant aliquam et qui. Nobis tibique id sea.</p>
 
 	<div style="height:200vh;"></div>
 </main>
