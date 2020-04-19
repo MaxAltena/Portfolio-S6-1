c281dfdc585198928d9ad9fa784341112e6bb384
Max Altena <Max@MaxAltena.com>
2020-04-19 18:54:25 +0200

    added init values

--- src/utils/stores.js
+++ src/utils/stores.js
@@ -2,9 +2,9 @@ import { writable } from "svelte/store";
 
 export const scrollY = writable(0);
 export const docCS = writable(window.appPreferences.colorScheme.current);
-export const name = writable("");
-export const semester = writable("");
-export const firstName = writable("");
-export const lastName = writable("");
-export const fullName = writable("");
+export const name = writable("Portfolio");
+export const semester = writable("S6");
+export const firstName = writable("Max");
+export const lastName = writable("Altena");
+export const fullName = writable("Max Altena");
 export const products = writable([]);
