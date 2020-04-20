42bfa9aa444f09f661f23b4c2914bc6fb2042811
Max Altena <Max@MaxAltena.com>
2020-04-20 20:12:15 +0200

    fixed product page

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -2,7 +2,11 @@
 	export let product;
 </script>
 
-{product.content === "" ? "<p>Huh, product niet gevonden? Probeer het later nog eens!</p>" : @html product.content}
+{#if product.content === ""}
+	<p>Huh, product niet gevonden? Probeer het later nog eens!</p>
+{:else}
+	{@html product.content}
+{/if}
 
 <style>
 
