5e334c9e9f61aef411784fa9547f7e7c625d7b31
Max Altena <Max@MaxAltena.com>
2020-04-21 02:01:25 +0200

    minor

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -22,11 +22,150 @@
 	<h2>Verantwoording</h2>
 	<p>Enkele eigen kritische vragen gemaakt en de uiteindelijke vragen aangescherpt en beter verwoord.</p>
 {:else if product.link === 'probleemanalyse'}
+	<h2>Aanleiding</h2>
+	<p>
+		Om een probleem op te lossen moet er eerst gekeken worden naar wat het probleem. Niet alleen dat er een probleem
+		is, maar welke dit is en waarom. Met deze informatie kan er beter worden gestuurd naar een juiste oplossing.
+	</p>
+	<h2>Aanpak</h2>
+	<p>
+		Na de kritische vragen en de antwoorden hierop ben ik met mijn eigen verdere aannames een analyse gaan maken van
+		het probleem. Hierbij zijn mogelijke oplossingen buiten beeld gebleven, omdat er in deze fase nog niet naar
+		oplossing maar naar problemen werd gezocht. De tijd die er in eerst instantie in werd gestoken door mij was niet
+		erg veel, zodat ik niet al te veel tijd had om er dieper over na te denken. Na het ingeleverd te hebben ben ik
+		nog eens terug gaan kijken en de aanpak verbeterd.
+	</p>
+	<h2>Conclusie</h2>
+	<p>
+		De analyse heeft me geholpen met op het eerste gezicht een probleem te zoeken. Door het kijken van een mogelijke
+		aanpak kon er ook direct een traject voor het project worden opgezet. Dit waren de eerste stapjes om tot een
+		goed project te komen.
+	</p>
+	<h2>Verantwoording</h2>
+	<p>
+		Ik heb mijn eigen probleemanalyse gemaakt en hier later op geïtereerd met de feedback van Michiel om mijn aanpak
+		iets te verduidelijken.
+	</p>
+{:else if product.link === 'projectplan'}
+	<h2>Aanleiding</h2>
+	<p>
+		Om het traject inzichtelijker te maken voor de opdrachtgever en de onderwijsinstelling is er een projectplan
+		gemaakt waarbij het duidelijk moet worden voor iedereen wat er de komende 9 weken zou worden gedaan. Niet alleen
+		handig voor andere, maar ook zeker voor jezelf.
+	</p>
+	<h2>Aanpak</h2>
+	<p>
+		Nadat ieder groepslid zijn eigen probleemanalyse had gemaakt werden al deze analyses bij elkaar gegooid om tot
+		een globale probleemanalyse te komen. Hiermee konden we een traject uitstippelen die we konden gaan volgen dit
+		project. Dit document werd opgesplitst in de groep, dus ieder groepslid ging een deel maken. Dit had zijn voor-
+		en nadelen, minder werk voor iedereen maar ook minder aan te tonen voor iedereen. Dit zou een wijze eerste les
+		worden voor het verloop van het project.
+	</p>
+	<h2>Conclusie</h2>
+	<p>
+		Het maken van het projectplan gaf duidelijkheid voor iedereen. Ookal vonden de docenten het een beetje vaag en
+		breed wat we zouden gaan doen. Dit zou later worden aangescherpt met een tweede versie van het document nadat er
+		voldoende onderzoek was gedaan. Deze vaagheid kwam vooral omdat we tijdens het maken van het projectplan nog
+		niet zeker wisten wat we nou precies gingen doen, en waren we dus van plan om meer onderzoek te gaan doen. De
+		volgende keer zouden we het projectplan dus moeten uitstellen tot we genoeg informatie hebben om een projectplan
+		te maken. Al betekent dit het teleurstellen van de docenten op het eerste gezicht, maar het beter aanpakken op
+		het lange termijn.
+	</p>
+	<h2>Verantwoording</h2>
+	<p>
+		Scope gemaakt voor het project. Meegeholpen met hoofd- en deelvragen aan te scherpen. Methode duidelijker
+		verwoord voor de groepsleden.
+	</p>
+{:else if product.link === 'doelgroep-en-publieksgroep'}
+	<h2>Aanleiding</h2>
+	<p>
+		Om meer inzicht te krijgen in de problemen die er bij de stichting speelden werd er door verschillende duo's
+		onderzoek gedaan. Naast het meer inzicht krijgen waren deze onderzoeken ook goed om ons projectplan aan te
+		scherpen.
+	</p>
+	<h2>Aanpak</h2>
+	<p>
+		Na het eerste projectplan waren er enkele deelvragen opgesteld om een globale hoofdvraag te beantwoorden. Een
+		van deze vragen: 'Wie is de doelgroep en de publieksgroep van Stichting Ik Wil?' moest worden beantwoord door
+		Bart van de Klundert en mijzelf. We ging kijken of de doel- en publieksgroep hetzelfde waren en of welke
+		verschillen en overeenkomsten ze hadden. Hiermee is er een duidelijker beeld van voor wie we het maken en is er
+		de mogelijkheid om problemen te ontdekken bij de publieksgroep. De onderzoeken die onder deze deelvragen vallen
+		zullen in hun eigen gedeelte worden toegelicht.
+	</p>
+	<h2>Conclusie</h2>
+	<p>
+		Het maken van een doordacht onderzoeksdocument kan redelijk wat tijd vergen. Het coördineren met je duo als deze
+		keer, want Bart was enkele keren afwezig, ging goed doordat we constant in contact waren. Mede hierdoor is er
+		geen dubbel werk gedaan en waren we op elk punt op 1 lijn. Uit het onderzoeksdocument zijn nuttige conclusies
+		getrokken voor de groep om mee verder te gaan, ookal hadden we de getrokken conclusie aan het begin niet
+		verwacht.
+	</p>
+	<h2>Verantwoording</h2>
+	<p>
+		Ik heb het global document opgezet waar Bart en ik in hebben zitten werken. Denk hierbij aan hypothese,
+		onderzoeksdoel, de verschillende methodes en conclusies.
+	</p>
+{:else if product.link === 'expert-interview'}
+	<h2>Aanleiding</h2>
+	<p>
+		Er is een expert interview gedaan om de verwachte doel- en publieksgroep te onthullen. Dit onderzoek komt voort
+		uit het onderzoek naar de doelgroep en de publieksgroep van Stichting Ik Wil.
+	</p>
+	<h2>Aanpak</h2>
+	<p>
+		Een interview met een expert op gebied van mensen, dan keek je direct Stephan Pauly aan, een van de oprichters
+		van de stichting. Samen met hem kon direct gekeken worden hoe het bij de stichting zat met mensen. Door zijn
+		inzichten over de publieksgroep en mogelijke verwachtingen van de doelgroep konden we ons onderzoek naar de
+		doel- en publieksgroep kickstarten. Ik moest daarintegen wel de juiste vragen stellen; niet te gesloten om ja
+		nee antwoorden te krijgen, maar ook niet te open om een verhaal te horen waar we niet al te veel aan hadden.
+	</p>
+	<h2>Conclusie</h2>
+	<p>
+		Uit het interview met Stephan kwam uit dat de doelgroep eigenlijk zo breed is als de mensen die er komen;
+		iedereen is er welkom. Dit was een antwoord dat onze aannames al een beetje bevestigden, de doelgroep is niet
+		echt gedefinieerd. Het interview zou de volgende keer moeten worden opgenomen, want dat was mij ontschoten.
+	</p>
+	<h2>Verantwoording</h2>
+	<p>Gehele document en onderzoek gedaan. Interview gedaan met Stephan Pauly.</p>
+{:else if product.link === 'publieksgroep-interviews'}
+	<h2>Aanleiding</h2>
+	<p>
+		Na het interview met Stephan moest er nog even goed gekeken worden naar wie de publieksgroep precies was.
+		Gelukkig waren ze niet zo heel ver weg, een enkele huiskamer van ons verwijderd. Met de informatie van wie de
+		publieksgroep is konden we verschillende persona's maken om onze toekomstige producten mee te valideren en
+		kijken of de doelgroep en de publieksgroep erg verschillende waren.
+	</p>
+	<h2>Aanpak</h2>
+	<p>
+		Wat doe je als je de publieksgroep wil leren kennen? Je gaat naar ze toe! Dit heb ik dan ook gedaan door middel
+		van 'interviews' te doen met ze. Interviews wil ik het niet noemen, omdat het zo'n groot en formeel gesprek
+		wordt. De gesprekken, informeel en persoonlijk, zijn een beter alternatief als je gezellig met een ander persoon
+		in de huiskamer een bakje koffie zit te drinken. Met deze gesprekken kom je niet alleen de standaard dingen te
+		weten, maar ook meer over de persoon zelf, wat waardevol is. Dit soort informatie kon goed gebruikt worden om te
+		kijken of de aannames van het gesprek met Stephan klopten.
+	</p>
+	<h2>Conclusie</h2>
+	<p>
+		Al snel kwam ik er achter dat de publieksgroep ook wel echt de doelgroep was. Mede door de diversiteit bij de
+		deelnemers waren er interessante gesprekken over van alles. Dit was erg fijn, zo leerde ik ze ook echt kennen.
+		Met de achtergrondverhalen over hoe ze hier terecht zijn gekomen konden gemakkelijk persona's worden gemaakt en
+		dit wekte verder bij mij empathie op. Als laatste konden we met deze gesprekkenvaststellen dat de doel- en
+		publieksgroep zo goed als hetzelfde waren. Hier lag dus ook geen probleem bij de stichting.
+	</p>
+	<h2>Verantwoording</h2>
+	<p>Gehele document gemaakt en onderzoek gedaan. Diverse mensen gesproken en leren kennen in de huiskamer.</p>
+{:else if product.link === 'brainstormsessie'}
+
 {:else if product.link === 'probleemanalyse'}
+
 {:else if product.link === 'probleemanalyse'}
+
 {:else if product.link === 'probleemanalyse'}
+
 {:else if product.link === 'probleemanalyse'}
+
 {:else if product.link === 'probleemanalyse'}
+
 {:else if product.link === 'probleemanalyse'}
 
 {:else if product.link === 'warzone' || product.link === 'super-secret'}
